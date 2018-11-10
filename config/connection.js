4c158e9da85ddf1ca1d4e7da3cf92c0204d4834d
Raynor Fisher <rfisher708@me.com>
2018-11-10 10:11:18 -0600

    Burgertime2

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,10 @@ var connection;
  {
   connection = mysql.createConnection({
     port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "burgers_db"
+    host: "lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "xtflwpv1fakm6jq8",
+    password: "acx3zn9sbi4cnjjq",
+    database: "	q7q1jmjoorfo08fn"
   });
 };
 //Handle errors
